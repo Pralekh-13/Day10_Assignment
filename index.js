2a1924738ab646c6a97db58616880384edbb9d54
PRALEKH RAMBHAD <106136815+Pralekh-13@users.noreply.github.com>
2023-06-08 22:12:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+function addButtonToContainer() {
+    
+    var button = document.createElement('button');
+    button.textContent = 'Click Me';
+  
+    
+    button.addEventListener('click', function() {
+      alert('Button clicked!');
+    });
+  
+    
+    var container = document.getElementById('container');
+    container.appendChild(button);
+  }
+  
+  
+  addButtonToContainer();
+  
